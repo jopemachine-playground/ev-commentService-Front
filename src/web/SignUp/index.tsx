8b73c65a7533ec63eb26ed77732f3c745fd54aa2
jopemachine <jopemachine@naver.com>
2019-12-30 22:10:37 +0900

    [feat] Add SignUp.tsx

--- src/web/SignUp/index.tsx
+++ src/web/SignUp/index.tsx
@@ -1,7 +1,33 @@
 import React, { useState } from "react";
-import { Form, FormGroup, Label, Input, Button } from "reactstrap";
+import { Alert, Form, FormGroup, Label, Input, Button } from "reactstrap";
 import "./SignUp.css";
 
 export default function SignUp() {
-    return <></>;
+
+  const [alertVisble, setAlertVisible] = useState<boolean>(true);
+  const [ID, setID] = useState<string>("");
+  const [PW, setPW] = useState<string>("");
+  const [PWConfirm, setPWConfirm] = useState<string>("");
+
+  return(
+    <div>
+      <Alert color="light" isOpen={alertVisble} toggle={() => setAlertVisible(false)}>
+        <strong>* </strong> 란은 필수입니다.
+      </Alert>
+      <Form>
+        <FormGroup>
+          <Label for="ID">ID</Label>
+          <Input type="text" name="ID" id="ID" />
+        </FormGroup>
+        <FormGroup>
+          <Label for="PW">PW</Label>
+          <Input type="password" name="PW" id="PW" />
+        </FormGroup>
+        <FormGroup>
+          <Label for="PW_Confirm">PW Confirm</Label>
+          <Input type="password" name="PW_Confirm" id="PW_Confirm" />
+        </FormGroup>
+      </Form>
+    </div>
+  );
 }
