2522b0f6d1a1250f3c9033b576ae26584ddd86e9
jopemachine <jopemachine@naver.com>
2019-12-30 19:26:44 +0900

    [chore] Update package.json to apply reactstrap

--- src/web/SignIn/index.tsx
+++ src/web/SignIn/index.tsx
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
-import { Form, FormGroup, Label, Input, Button } from "reactstrap";
+import { Form, FormGroup, Label, Input, Button, Alert } from "reactstrap";
 import "./SignIn.css";
 
 export default function SignIn() {
 
   const [ID, setID] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
+  const [PW, setPW] = useState<string>("");
 
   function validateForm() {
-    return ID.length > 0 && password.length > 0;
+    return ID.length > 0 && PW.length > 0;
   }
 
   function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
@@ -20,22 +20,23 @@ export default function SignIn() {
       <section>
         <p id="Title" className="lead">Login</p>
         <p id="Title-lead" className="lead">감정 분석 댓글 서비스를 이용하기 위해 로그인하세요.</p>
-
         <Form>
           <FormGroup>
-            <Label for="ID">ID: </Label>
-            <Input type="text" name="ID" id="ID" placeholder={"write your ID"}></Input>
+            <Label for={"ID"}>ID: </Label>
+            <Input value={ID} onChange={(e: React.SyntheticEvent<HTMLInputElement>) => setID(e.currentTarget.value)} type="text" name="ID" id="ID" placeholder={"write your ID"}></Input>
           </FormGroup>
           <FormGroup>
-            <Label for="PW">PW: </Label>
-            <Input type="password" name="PW" id="PW" placeholder={"write your PW"}></Input>
+            <Label for={"PW"}>PW: </Label>
+            <Input value={PW} onChange={(e: React.SyntheticEvent<HTMLInputElement>) => setPW(e.currentTarget.value)} type="password" name="PW" id="PW" placeholder={"write your PW"}></Input>
           </FormGroup>
         </Form>
 
-        <Button type="submit" className="btn btn-dark btn-block">
+        <Button color={"primary"} type={"submit"} onClick={handleSubmit}>
           로그인
         </Button>
       </section>
     </div>
   );
 }
+
+
