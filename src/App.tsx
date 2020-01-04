8bb29e7000dc2095f546d0c76b3e837dfd982344
jopemachine <jopemachine@naver.com>
2020-01-04 10:04:12 +0900

    [feat] Update App.tsx to add react-alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,38 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import RouterApp from './web/RouterApp';
+import { transitions, positions, Provider as AlertProvider } from 'react-alert';
 import { UseSessionProvider } from 'react-session-hook';
 import './App.css';
 
+const alertOption = {
+  position: positions.BOTTOM_CENTER,
+  timeout: 5000,
+  offset: '30px',
+  transition: transitions.SCALE
+};
+
+const AlertTemplate = ({ style, options, message, close }) => (
+  <div style={style}>
+    {options.type === 'info' && '!'}
+    {options.type === 'success' && ':)'}
+    {options.type === 'error' && ':('}
+    {message}
+    <button onClick={close}>X</button>
+  </div>
+)
+
 const App: React.FC = () => {
   return (
-  <UseSessionProvider>
-    <div className="App">
-      <BrowserRouter>
-      <RouterApp />
-     </BrowserRouter>
-    </div>
-  </UseSessionProvider>
+    <AlertProvider template={AlertTemplate} {...alertOption}>
+      <UseSessionProvider>
+        <div className="App">
+          <BrowserRouter>
+          <RouterApp />
+         </BrowserRouter>
+        </div>
+      </UseSessionProvider>
+    </AlertProvider>
   );
 }
 
